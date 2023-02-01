6fe496695b71fa2ebc6e292e55297e7d8b360949
Parthe Pandit <parthe@users.noreply.github.com>
2023-02-01 13:12:22 -0800

    Added setup.py to make repo pip installable

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='rfm',
+    version='2.0.0',
+    author='Adityanarayanan Radhakrishnan, Daniel Beaglehole, Parthe Pandit',
+    author_email='aradha@mit.edu, dbeaglehole@ucsd.edu, parthepandit@ucsd.edu',
+    description='Recursive procedure for Task-specific Feature Learning with Kernel predictors',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/aradha/recursive_feature_machines',
+    project_urls = {
+        "Bug Tracker": "https://github.com/aradha/recursive_feature_machines/issues"
+    },
+    license='MIT license',
+    packages=['rfm'],
+    install_requires=[
+      'pytorch>=1.13.0',
+      'torchvision>=0.14.0',
+      'hickle>=5.0.2',
+      'tqdm>=4.64.1'
+    ],
+)
