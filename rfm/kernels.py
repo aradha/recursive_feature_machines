fixed numerical instability in EGOP implementation

rfm/kernels.py
@@ -18,17 +18,16 @@ def euclidean_distances(samples, centers, squared=True):
 
     return distances
 
-
 def euclidean_distances_M(samples, centers, M, squared=True):
     
-    samples_norm2 = torch.einsum('md, mD, dD -> m', samples, samples, M)
+    samples_norm2 = ((samples @ M) * samples).sum(-1)
 
     if samples is centers:
         centers_norm2 = samples_norm2
     else:
-        centers_norm2 = torch.einsum('md, mD, dD -> m', centers, centers, M)
+        centers_norm2 = ((centers @ M) * centers).sum(-1)
 
-    distances = -2 * torch.einsum('md, nD, dD -> mn', samples, centers, M)
+    distances = -2 * (samples @ M) @ centers.T
     distances.add_(samples_norm2.view(-1, 1))
     distances.add_(centers_norm2)
 
@@ -36,6 +35,7 @@ def euclidean_distances_M(samples, centers, M, squared=True):
         distances.clamp_(min=0).sqrt_()
 
     return distances
+
 
 def laplacian(samples, centers, bandwidth):
     '''Laplacian kernel.

rfm/recursive_feature_machine.py
@@ -1,5 +1,5 @@
 from eigenpro2 import KernelModel
-import torch
+import torch, numpy as np, kernels
 from tqdm import tqdm
 import hickle
 
@@ -13,53 +13,23 @@ else:
 
 class RecursiveFeatureMachine:
 
-    def __init__(self, kernel_fn, kernel_grad1, per_class=False):
+    def __init__(self):
         self.M = None
-        self.per_class_str = 'c' if per_class else ''
-        self.kernel = lambda x, z: kernel_fn(x, z, self.M) # must take 3 arguments (x, z, M)
-        self.kernel_grad1 = lambda x, z: kernel_grad1(x, z, self.M)
         self.model = None
         self.device = DEVICE
         self.mem_gb = DEV_MEM_GB
 
-
     def get_data(self, data_loader):
-        X = []
-        y = []
+        X, y = [], []
         for idx, batch in enumerate(data_loader):
             inputs, labels = batch
             X.append(inputs)
             y.append(labels)
         return torch.cat(X, dim=0), torch.cat(y, dim=0)
 
-    def reduce(self, samples, centers, Ksc, alpha):
-
-        M1 = torch.einsum(
-            f"md, mn, nc, mD, mn, nc -> {self.per_class_str}dD", 
-            samples, Ksc, alpha, 
-            samples, Ksc, alpha
-            )
-        M2 = torch.einsum(
-            f"nd, mn, nc, nD, mn, nc -> {self.per_class_str}dD", 
-            centers, Ksc, alpha,
-            centers, Ksc, alpha
-        )
-        M12 = torch.einsum(
-            f"md, mn, nc, nD, mn, nc -> {self.per_class_str}dD", 
-            samples, Ksc, alpha,
-            centers, Ksc, alpha
-        )
-        return (M1 + M2 - M12 - M12.swapaxes(-1, -2))/samples.shape[0]
-
     def update_M(self):
-        num_samples = 20000
-        indices = torch.randperm(len(self.centers))[:num_samples]
-        samples = self.centers[indices]
+        raise NotImplementedError("Must implement this method in a subclass")
 
-        K = self.kernel_grad1(samples, self.centers)
-
-        self.M = self.reduce(samples, self.centers, K, self.weights)
-        
 
     def fit_predictor(self, centers, targets, **kwargs):
         self.centers = centers
@@ -72,7 +42,11 @@ class RecursiveFeatureMachine:
 
 
     def fit_predictor_lstsq(self, centers, targets):
-        return torch.linalg.lstsq(self.kernel(centers, centers), targets)[0]
+        return torch.from_numpy(np.linalg.solve(
+            self.kernel(centers, centers) 
+            + 1e-3*torch.eye(len(centers)), 
+            targets
+        ))
 
 
     def fit_predictor_eigenpro(self, centers, targets, **kwargs):
@@ -90,8 +64,10 @@ class RecursiveFeatureMachine:
             iters=3, name=None, reg=1e-3, method='lstsq', 
             train_acc=False, loader=True, classif=True):
         if method=='eigenpro':
+            raise NotImplementedError(
+                "EigenPro method is not yet supported. "+
+                "Please try again with `method='lstlq'`")
             #self.fit_using_eigenpro = (method.lower()=='eigenpro')
-            raise NotImplementedError("EigenPro method is not yet supported. Please try again with `method='lstlq'`")
         self.fit_using_eigenpro = False
         
         if loader:
@@ -113,17 +89,16 @@ class RecursiveFeatureMachine:
 
 
             test_mse = self.score(X_test, y_test, metric='mse')
-            print(f"Round {i}, Test MSE: {test_mse:.2f}")
+            print(f"Round {i}, Test MSE: {test_mse:.4f}")
             
-
-            self.update_M()
+            self.update_M(X_train)
 
             if name is not None:
                 hickle.dump(M, f"saved_Ms/M_{name}_{i}.h")
 
         self.fit_predictor(X_train, y_train)
         final_mse = self.score(X_test, y_test, metric='mse')
-        print(f"Final MSE: {final_mse:.2f}")
+        print(f"Final MSE: {final_mse:.4f}")
         if classif:
             final_test_acc = self.score(X_test, y_test, metric='accuracy')
             print(f"Final Test Acc: {final_test_acc:.2f}%")
@@ -136,3 +111,74 @@ class RecursiveFeatureMachine:
             return (1.*(targets.argmax(-1) == preds.argmax(-1))).mean()*100.
         elif metric=='mse':
             return (targets - preds).pow(2).mean()
+
+
+class LaplaceRFM(RecursiveFeatureMachine):
+
+    def __init__(self, bandwidth=1.):
+        super(LaplaceRFM, self).__init__()
+        self.bandwidth = bandwidth
+        self.kernel = lambda x, z: laplacian_M(x, z, self.M, self.bandwidth) # must take 3 arguments (x, z, M)
+        
+
+    def update_M(self, samples):
+        
+        K = self.kernel(samples, self.centers)
+
+        dist = kernels.euclidean_distances_M(samples, self.centers, self.M, squared=False)
+        dist = torch.where(dist < 1e-10, torch.zeros(1).float(), dist)
+
+        K = K/dist
+        K[K == float("Inf")] = 0.
+
+        n, d = self.centers.shape
+        n, c = self.weights.shape
+        m, d = samples.shape
+        print(m, n, c, d)
+
+        step2 = (K @ (
+            self.weights.view(n, c, 1) * (self.centers @ self.M).reshape(n, 1, d)
+        ).view(n, c*d)
+        ).view(m, c, d)
+
+        step3 = K @ self.weights
+
+
+        step3 = step3.reshape(m, c, 1)
+        x1 = (samples @ self.M).reshape(m, 1, d)
+        step3 = step3 * x1
+
+        G = (step2 - step3) / L
+        self.M = torch.einsum('ncd, ncD -> dD', G, G)/len(samples)
+        print(self.M)
+
+
+
+if __name__ == "__main__":
+    import numpy as np, torch
+    from kernels import laplacian_M, laplacian_M_grad1
+    torch.set_default_dtype(torch.float64)
+    
+    # define target function
+    def fstar(X):
+        return (X[:, 0]  > 0)[:,None]
+
+    # create low rank data
+    n = 4000
+    d = 3
+    L = 1 # bandwidth
+    np.random.seed(0)
+    X_train = torch.from_numpy(np.random.normal(scale=0.5, size=(n,d)))
+    X_test = torch.from_numpy(np.random.normal(scale=0.5, size=(n,d)))
+
+    y_train = fstar(X_train).double()
+    y_test = fstar(X_test).double()
+
+    model = LaplaceRFM()
+    model.fit(
+        (X_train, y_train), 
+        (X_test, y_test), 
+        loader=False,
+        iters=5,
+        classif=False
+    )