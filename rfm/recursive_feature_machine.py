de1b1ef8cd393919ce8d5a47702db5b819c4f8be
dmbeaglehole <84294223+dmbeaglehole@users.noreply.github.com>
2025-01-25 18:46:02 -0800

    Update recursive_feature_machine.py

--- rfm/recursive_feature_machine.py
+++ rfm/recursive_feature_machine.py
@@ -100,7 +100,7 @@ class RecursiveFeatureMachine(torch.nn.Module):
         if class_weight is not None and self.fit_using_eigenpro:
             raise ValueError("Class weights are not supported for EigenPro")
 
-        if class_weight == 'inverse':
+        if verbose and class_weight == 'inverse':
             print("Weighting samples by inverse class frequency")
         
         if loader:
