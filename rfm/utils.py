098812153c357ec4a5525eb201300c9964a53d47
Daniel Beaglehole <dbeaglehole@ucsd.edu>
2025-03-05 13:42:48 -0600

    fixed assert

--- rfm/utils.py
+++ rfm/utils.py
@@ -8,12 +8,12 @@ def float_x(data):
 
 def matrix_sqrt(M, agop_power=0.5):
     if len(M.shape) == 2:
-        assert(M.shape[0] == M.shape[1], "Matrix must be square")
+        assert M.shape[0] == M.shape[1], "Matrix must be square"
         S, U = torch.linalg.eigh(M)
         S[S<0] = 0.
         return U @ torch.diag(S**agop_power) @ U.T
     elif len(M.shape) == 1:
-        assert(M.shape[0] > 0, "Vector must be non-empty")
+        assert M.shape[0] > 0, "Vector must be non-empty"
         M[M<0] = 0.
         return M**agop_power
     else:
