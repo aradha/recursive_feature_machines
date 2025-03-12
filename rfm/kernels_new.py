fixed numerical issues and wrote a RFM wrapper class

rfm/kernels_new.py
@@ -48,25 +48,31 @@ class Kernel:
                            mat: Optional[torch.Tensor] = None) -> torch.Tensor:
         """
         Return the matrix of function gradients at points z.
-        The function is given by \sum_i coefs[i] * k(x[i], \cdot).
+        The function is given by f_l(\cdot) = \sum_i coefs[l, i] * k(x[i], \cdot).
         :param x: Matrix of shape (n_x, d_in)
         :param z: Matrix of shape (n_z, d_in)
-        :param coefs: Vector of shape (n_x,)
+        :param coefs: Vector of shape (f, n_x) where f is the number of functions
         :param mat: Matrix of shape (d_in, d_out) or vector of shape (d_in)
-        :return: Should return a tensor of shape (n_z, d_in).
+        :return: Should return a tensor of shape (f, n_z, d_in).
         """
         grads = self._get_function_grad_impl(self._transform_m(x, mat), self._transform_m(z, mat), coefs)
         return self._transform_m(grads, mat)
 
     def get_agop(self, x: torch.Tensor, z: torch.Tensor, coefs: torch.Tensor,
                  mat: Optional[torch.Tensor] = None) -> torch.Tensor:
+        # see get_function_grads
         f_grads = self.get_function_grads(x, z, coefs, mat)
-        return f_grads.t() @ f_grads
+        # merge output and n_z dims
+        f_grads = f_grads.reshape(-1, f_grads.shape[-1])
+        return f_grads.transpose(-1, -2) @ f_grads
 
     def get_agop_diag(self, x: torch.Tensor, z: torch.Tensor, coefs: torch.Tensor,
                       mat: Optional[torch.Tensor] = None) -> torch.Tensor:
+        # see get_function_grads
         f_grads = self.get_function_grads(x, z, coefs, mat)
-        return f_grads.square().sum(dim=0)
+        # merge output and n_z dims
+        f_grads = f_grads.reshape(-1, f_grads.shape[-1])
+        return f_grads.square().sum(dim=-2)
 
 
 class LaplaceKernel(Kernel):
@@ -91,8 +97,8 @@ class LaplaceKernel(Kernel):
 
         # gradient of k(x, z) = exp(-\gamma \|x - z\|^\beta) wrt z  (where \beta = self.exponent)
         # is -\gamma k(x, z) \beta \|x - z\|^{\beta - 1} (z-x)/\|x-z\| = -\gamma \beta k(x, z) \|x - z\|^{\beta-2} (z-x)
-        # therefore, setting f(z) = \sum_i coefs[i] k(x[i], z), we have
-        # \grad f(z[j]) = \sum_i coefs[i] M[i, j] (z[j] - x[i]),
+        # therefore, setting f_l (z) = \sum_i coefs[l, i] k(x[i], z), we have
+        # \grad f_l(z[j]) = \sum_i coefs[l, i] M[i, j] (z[j] - x[i]),
         # where M[i, j] = -\gamma \beta k(x[i], z[j]) \|x[i] - z[j]\|^{\beta - 2}
         gamma = 1. / self.bandwidth
         kernel_mat = dists ** self.exponent
@@ -106,10 +112,12 @@ class LaplaceKernel(Kernel):
         kernel_mat.mul_(dists)
         kernel_mat.mul_(-gamma * self.exponent)
 
-        # now we want result[j, d] = \sum_i coefs[i] grad_mat[i, j] (z[j, d] - x[i, d])
-        kernel_mat.mul_(coefs[:, None])
+        # now we want result[l, j, d] = \sum_i coefs[l, i] M[i, j] (z[j, d] - x[i, d])
+        return torch.einsum('li,ij,ijd->ljd', coefs, kernel_mat, (z[None, :, :] - x[:, None, :]))
 
-        return kernel_mat.sum(dim=0)[:, None] * z - kernel_mat.t() @ x
+        # these computations would be more memory-efficient but are too unstable numerically
+        # return (coefs @ kernel_mat)[:, :, None] * z[None, :, :] - torch.einsum('li,id,ij->ljd', coefs, x, kernel_mat)
+        # return torch.einsum('li,ij,jd->ljd', coefs, kernel_mat, z) - torch.einsum('li,ij,id->ljd', coefs, kernel_mat, x)
 
 
 if __name__ == '__main__':
@@ -117,14 +125,14 @@ if __name__ == '__main__':
 
     x = torch.linspace(-2.0, 2.0, 5)[:, None]
     z = torch.linspace(-4.0, 4.0, 500)[:, None]
-    coefs = torch.as_tensor([1.0, 0.8, 0.4, -0.5, -2.0])
-    kernel = LaplaceKernel(bandwidth=2.0, exponent=1.2)
+    coefs = torch.as_tensor([1.0, 0.8, 0.4, -0.5, -2.0])[None, :]
+    kernel = LaplaceKernel(bandwidth=2.0, exponent=1.0)
     # mat = None
     mat = torch.as_tensor([0.5])
     # mat = torch.as_tensor([[0.5]])
-    f_values = coefs @ kernel.get_kernel_matrix(x, z, mat)
+    f_values = coefs[0, :] @ kernel.get_kernel_matrix(x, z, mat)
     plt.plot(z[:, 0], f_values, 'tab:blue', label='function')
-    plt.plot(z, kernel.get_function_grads(x, z, coefs, mat), 'tab:orange', label='gradient')
+    plt.plot(z, kernel.get_function_grads(x, z, coefs, mat).squeeze(0), 'tab:orange', label='gradient')
     plt.plot(0.5 * (z[1:, 0] + z[:-1, 0]), (f_values[1:] - f_values[:-1]) / (z[1:, 0] - z[:-1, 0]), color='tab:green',
              linestyle='--', label='finite diff')
     plt.legend()

rfm/recursive_feature_machine.py
@@ -1,3 +1,4 @@
+from rfm.kernels_new import Kernel
 from .eigenpro import KernelModel
     
 import torch, numpy as np
@@ -52,7 +53,6 @@ class RecursiveFeatureMachine(torch.nn.Module):
                                                        **kwargs)
         else:
             self.weights = self.fit_predictor_lstsq(centers, targets, class_weight=class_weight, solver=solver)
-
 
     def fit_predictor_lstsq(self, centers, targets, class_weight=None, solver='solve'):
         centers = centers.to(self.device)
@@ -111,7 +111,7 @@ class RecursiveFeatureMachine(torch.nn.Module):
             solver='solve', **kwargs):
                 
         self.fit_using_eigenpro = (method.lower()=='eigenpro')
-        use_sqrtM = self.kernel_type in ['laplacian_gen']
+        use_sqrtM = self.kernel_type in ['laplacian_gen', 'generic']
         
         if iters is None:
             iters = self.iters
@@ -425,6 +425,30 @@ class GeneralizedLaplaceRFM(RecursiveFeatureMachine):
                                     self.M_batch_size, 
                                     self.diag
                                     )
+        return agop
+
+
+class GenericRFM(RecursiveFeatureMachine):
+    def __init__(self, kernel: Kernel, agop_power=0.5, **kwargs):
+        super().__init__(**kwargs)
+        self.kernel_obj = kernel
+        self.kernel = lambda x, z: self.kernel_obj.get_kernel_matrix(x, z, self.sqrtM)
+        self.kernel_type = 'generic'
+        self.agop_power = agop_power
+
+    def update_M(self, samples, p_batch_size):
+        if self.M is None:
+            if self.diag:
+                self.M = torch.ones(samples.shape[-1], device=samples.device, dtype=samples.dtype)
+                self.sqrtM = torch.ones(samples.shape[-1], device=samples.device, dtype=samples.dtype)
+            else:
+                self.M = torch.eye(samples.shape[-1], device=samples.device, dtype=samples.dtype)
+                self.sqrtM = torch.eye(samples.shape[-1], device=samples.device, dtype=samples.dtype)
+
+        samples = samples.to(self.device)
+        self.centers = self.centers.to(self.device)
+        agop_func = self.kernel_obj.get_agop_diag if self.diag else self.kernel_obj.get_agop
+        agop = agop_func(x=self.centers, z=samples, coefs=self.weights.t(), mat=self.sqrtM)
         return agop
 
 

test.py
@@ -1,17 +1,24 @@
+import numpy as np
 import torch
 from rfm import LaplaceRFM
+from rfm.kernels_new import LaplaceKernel
+from rfm.recursive_feature_machine import GenericRFM
+
+np.random.seed(0)
+torch.manual_seed(0)
 
 def fstar(X):
     return torch.cat([
-            (X[:, 0]  > 0)[:,None], 
+            (X[:, 0]  > 0)[:,None],
     	    (X[:, 1] < 0.5)[:, None]], 
     	    axis=1
         ).float()
 
-model =LaplaceRFM(bandwidth=1.)
+model = LaplaceRFM(bandwidth=1., diag=False)
+# model = GenericRFM(LaplaceKernel(bandwidth=1., exponent=0.8), diag=True)
 
 n = 4000 # samples
-d = 100  # dimension
+d = 4  # dimension
 c = 2    # classes
 
 X_train = torch.randn(n, d)