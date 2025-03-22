708a3475ac5b8ae21d24c45d49547ea710590ec0
Daniel Beaglehole <dbeaglehole@ucsd.edu>
2025-03-21 19:33:02 -0500

    default to stable matrix power

--- rfm/utils.py
+++ rfm/utils.py
@@ -14,19 +14,20 @@ def matrix_power(M, power):
     :param power: Power to raise the matrix to.
     :return: Matrix raised to the power - M^{power}.
     """
-    if len(M.shape) == 2:
-        assert M.shape[0] == M.shape[1], "Matrix must be square"
+    return stable_matrix_power(M, power)
+    # if len(M.shape) == 2:
+    #     assert M.shape[0] == M.shape[1], "Matrix must be square"
 
-        # gpu square root
-        S, U = torch.linalg.eigh(M)
-        S[S<0] = 0.
-        return U @ torch.diag(S**power) @ U.T
-    elif len(M.shape) == 1:
-        assert M.shape[0] > 0, "Vector must be non-empty"
-        M[M<0] = 0.
-        return M**power
-    else:
-        raise ValueError(f"Invalid matrix shape for square root: {M.shape}")
+    #     # gpu square root
+    #     S, U = torch.linalg.eigh(M)
+    #     S[S<0] = 0.
+    #     return U @ torch.diag(S**power) @ U.T
+    # elif len(M.shape) == 1:
+    #     assert M.shape[0] > 0, "Vector must be non-empty"
+    #     M[M<0] = 0.
+    #     return M**power
+    # else:
+    #     raise ValueError(f"Invalid matrix shape for square root: {M.shape}")
     
 def stable_matrix_power(M, power):
     """
@@ -37,11 +38,12 @@ def stable_matrix_power(M, power):
     """
     if len(M.shape) == 2:
         assert M.shape[0] == M.shape[1], "Matrix must be square"
-
+        M_cpu = M.cpu()
+        M_cpu.diagonal().add_(1e-8)
         if power==0.5:
-            return sqrtm(M.cpu()).to(M.device)
+            return sqrtm(M_cpu).to(M.device)
         else:
-            return fractional_matrix_power(M.cpu(), power).to(M.device)
+            return fractional_matrix_power(M_cpu, power).to(M.device)
     elif len(M.shape) == 1:
         assert M.shape[0] > 0, "Vector must be non-empty"
         M[M<0] = 0.
